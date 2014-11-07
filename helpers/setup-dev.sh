a41c84d0e110f738c66982558f677aca2f54f2e1
George Lestaris <glestaris@gmail.com>
2014-11-07 10:57:58 +0200

    Updates setup-dev.sh after removing src directory.

--- helpers/setup-dev.sh
+++ helpers/setup-dev.sh
@@ -2,5 +2,5 @@
 BASE_PATH=$(cd $(dirname $BASH_SOURCE)/.. && pwd)
 
 . ~/ve/iCE/bin/activate
-export PYTHONPATH="$PYTHONPATH:$BASE_PATH/src"
+export PYTHONPATH="$PYTHONPATH:$BASE_PATH"
 export ICE_CONFIG_PATH="$BASE_PATH/etc"
