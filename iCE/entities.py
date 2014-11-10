907c65c0f3300a659dab177a2c5a0fa8e47247d8
George Lestaris <glestaris@gmail.com>
2014-11-10 18:57:18 +0200

    Some refactoring of the Instance class

--- iCE/entities.py
+++ iCE/entities.py
@@ -1,40 +1,41 @@
 class Instance(object):
-
-    #
-    # SSH key attribute values
-    #
-
-    SSH_KEY_FINGERPRINT_DEFAULT = 1
-
     #
     # Status attribute values
     #
 
-    STATUS_UNKNOWN = 0
-    STATUS_RUNNING = 1
-    STATUS_UNREACHABLE = 2
-    STATUS_BANNED = 3
+    STATUS_UNKNOWN = 'unknown'
+    STATUS_RUNNING = 'running'
+    STATUS_UNREACHABLE = 'unreachable'
+    STATUS_BANNED = 'banned'
 
     #
     # Constructor
     #
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         # Identification
-        self.uuid = None  # TODO
+        self.id = None
 
         # Networking
-        self.public_ip_address = None
-        self.private_ip_address = None
+        self.networks = []
+        for net in kwargs.get('networks', []):
+            my_net = {
+                'addr': net['addr']
+            }
+            if net.has('iface'):
+                my_net['iface'] = net['iface']
+            if net.has('bcast_addr'):
+                my_net['bcast_addr'] = net['bcast_addr']
+            self.networks.append(my_net)
 
         # Cloud info
-        self.cloud_endpoint = None
-        self.security_group_id = None
+        self.cloud_id = kwargs.get('cloud_id', None)
+        self.vpc_id = kwargs.get('vpc_id', None)
 
         # SSH options
-        self.ssh_user = 'root'
-        self.ssh_key_fingerprint = Instance.SSH_KEY_FINGERPRINT_DEFAULT
+        self.ssh_username = kwargs.get('ssh_username', 'root')
+        self.ssh_authorized_fingerprint = kwargs['ssh_authorized_fingerprint']
 
         # Instance status
         self.failed_pings_count = 0
-        self.status = Instance.STATUS_UNKNOWN
+        self.status = Instance.STATUS_RUNNING
