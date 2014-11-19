2c779d41fc8275b4905368f0c67904b98437fa8f
George Lestaris <glestaris@gmail.com>
2014-11-19 23:35:56 +0200

    Adds classes for tasks

--- ice/__init__.py
+++ ice/__init__.py
@@ -1,3 +1,4 @@
 """Entry point for the iCE application."""
 from .api_client import APIClient
 from .entities import Instance
+from .tasks import Callable, Runner, Task, ParallelTask
---
+++ ice/tasks.py
@@ -0,0 +1,30 @@
+"""Fabric - iCE tasks."""
+from fabric import api
+
+
+class Callable(object):
+
+    def __init__(self, func):
+        self.func = func
+        self.help_msg = func.__doc__
+
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+
+class Runner(Callable):
+    pass
+
+
+class Task(Callable):
+
+    def __init__(self, func):
+        super(Task, self).__init__(func)
+        self.func = api.task(func)
+
+
+class ParallelTask(Task):
+
+    def __init__(self, func):
+        super(ParallelTask, self).__init__(func)
+        self.func = api.parallel(func)
