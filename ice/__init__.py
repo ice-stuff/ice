d5bd36fe7ca519129b02b0aa24c58f5663eeb0b4
George Lestaris <glestaris@gmail.com>
2014-12-05 17:02:26 +0200

    ice exposes Session clasS

--- ice/__init__.py
+++ ice/__init__.py
@@ -1,4 +1,4 @@
 """Entry point for the iCE application."""
 from .api_client import APIClient
-from .entities import Instance
+from .entities import Instance, Session
 from .tasks import Callable, Runner, Task, ParallelTask
