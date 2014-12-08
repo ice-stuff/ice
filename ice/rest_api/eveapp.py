5b2ab2f627245e0e20c3ebaf60269008f1181de7
George Lestaris <glestaris@gmail.com>
2014-12-08 17:39:12 +0200

    Removes component from get_configuration call

--- ice/rest_api/eveapp.py
+++ ice/rest_api/eveapp.py
@@ -18,7 +18,7 @@ class APIServer(Eve):
 
     def __init__(self, *args, **kwargs):
         # iCE
-        self.cfg = config.get_configuration()
+        self.cfg = config.get_configuration('api_server')
         in_debug = self.cfg.get_bool('api_server', 'debug', False)
         self.apiLogger = logging.get_logger('ice')
         if in_debug:
