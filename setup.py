20efb1b584801c681fee768e3dd68f059b5a3214
George Lestaris <glestaris@gmail.com>
2015-08-07 23:20:07 +0100

    Add nosetests package

--- setup.py
+++ setup.py
@@ -22,7 +22,8 @@ setuptools.setup(
         "IPython>=2.3.0",
         "requests>=1.2.0",
         "fabric>=1.10.0",
-        "boto>=2.34.0"
+        "boto>=2.34.0",
+        "nose>=1.3.0"
     ],
 
     # Configuration
