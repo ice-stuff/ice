689c0330463836f4f4d40eff6d8747fd23039df1
George Lestaris <glestaris@gmail.com>
2015-08-07 23:56:00 +0100

    Remove setup-dev script

--- helpers/setup-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-BASE_PATH=$(cd $(dirname $BASH_SOURCE)/.. && pwd)
-
-. ~/ve/iCE/bin/activate
-export PYTHONPATH="$PYTHONPATH:$BASE_PATH"
-export ICE_CONFIG_PATHS="$BASE_PATH/config/default:$BASE_PATH/config/local"
