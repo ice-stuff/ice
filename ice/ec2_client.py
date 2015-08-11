769758670f8e5df38e2686dd7c99fdc76291bd32
George Lestaris <glestaris@gmail.com>
2015-08-11 07:35:00 +0100

    Add tests for ice.ec2_client

--- ice/ec2_client.py
+++ ice/ec2_client.py
@@ -1,19 +1,18 @@
 """EC2-like cloud related helper commands."""
-import time
-
 from boto import ec2
 from boto import exception as boto_exception
 
 
 class EC2CloudAuth(object):
-    """EC2 cloud authentication parameters
-
-    :type ec2_url: string
-    :type aws_access_key: string
-    :type aws_secret_key: string
-    """
+    """EC2 cloud authentication parameters"""
 
     def __init__(self, ec2_url, aws_access_key, aws_secret_key):
+        """Create an EC2 auth object.
+
+        :param ec2_url string: EC2 URL or region.
+        :param aws_access_key string: AWS access key.
+        :param aws_secret_key string: AWS secret key.
+        """
         self.ec2_url = ec2_url
         self.aws_access_key = aws_access_key
         self.aws_secret_key = aws_secret_key
@@ -30,68 +29,77 @@ class EC2CloudAuth(object):
         return self._conn
 
 
+class EC2VMSpec(object):
+    """Specification of an EC2 VM"""
+
+    def __init__(self, ami_id, ssh_key_name, flavor='t2.micro',
+                 user_data=None, security_group_id=None,
+                 security_group_name=None, subnet_id=None):
+        """Create an EC2 VM specification
+
+        :param str ami_id: AWS image (AMI) id.
+        :param str ssh_key_name: Name of the AWS SSH keypair.
+        :param str flavor: AWS flavor. Optional, default: t2.micro.
+        :param str user_data: User (contextualisation) data to pass over.
+            Optional, default: None.
+        :param str security_group_id: Id of AWS security group. Optional:
+            default: None.
+        :param str security_group_name: Name of AWS security group.
+            Optional, default: None.
+        :param str subnet_id: Id of the AWS subnet. Optional, default:
+            None.
+        """
+        self.ami_id = ami_id
+        self.ssh_key_name = ssh_key_name
+        self.flavor = flavor
+        self.user_data = user_data
+        self.security_group_id = security_group_id
+        self.security_group_name = security_group_name
+        self.subnet_id = subnet_id
+
+
 class EC2Client(object):
+    """EC2-like API client."""
 
-    """EC2-like API client.
+    def __init__(self, auth, logger):
+        """Create an EC2 client for a given authentication.
 
-    :type config: ice.config.Configuration
-    :type logger: logging.logger
-    :type clouds: dict
-    :type default_cloud_id: str
-    """
-
-    def __init__(self, config, logger):
-        self.config = config
+        :param ice.ec2_client.EC2CloudAuth auth: EC2 authentication parameters.
+        :param logging.Logger logger: Logger.
+        """
+        self.auth = auth
         self.logger = logger
 
-        # Parse configuration
-        self.clouds, self.default_cloud_id = self._parse_cloud_configuration()
-
-    def create(self, amt, user_data=None, ami_id=None, flavor=None,
-               cloud_id=None):
+    def create(self, amt, spec):
         """Creates a list of VMs.
 
         :param int amt: The number of VMs to create.
-        :param str user_data: The user data to pass to the VM.
-        :param str ami_id: The AMI Id to use.
-        :param str flavor: The instances flavor (instance type).
-        :param str cloud_id: The id of the cloud to use.
-        :rtype: boto.ec2.instance.Reservation
+        :param ice.ec2_client.EC2VMSpec spec: The specification of the VMs to
+            launch.
         :return: The reservation or `None` in case of error.
+        :rtype: boto.ec2.instance.Reservation
         """
-        # Look for cloud
-        if cloud_id is None:
-            cloud_id = self.default_cloud_id
-        cloud = self.clouds.get(cloud_id, None)
-        if cloud is None:
-            self.logger.error('Failed to use cloud {}'.format(cloud_id))
-            return None
-
-        # Define arguments
-        if ami_id is None:
-            ami_id = cloud['default_ami_id']
         boto_kwargs = {
-            'instance_type': flavor,
-            'key_name': cloud['ssh_key_name'],
+            'instance_type': spec.flavor,
+            'key_name': spec.ssh_key_name,
             'min_count': amt,
             'max_count': amt
         }
-        if boto_kwargs['instance_type'] is None:
-            boto_kwargs['instance_type'] = cloud['default_flavor']
-        if user_data is not None:
-            boto_kwargs['user_data'] = user_data
-        if cloud['sg_name'] is not None:
-            boto_kwargs['security_groups'] = [cloud['sg_name']]
-        if ['sg_id'] is not None:
-            boto_kwargs['security_group_ids'] = [cloud['sg_id']]
-        if cloud['subnet_id'] is not None:
-            boto_kwargs['subnet_id'] = cloud['subnet_id']
-        # if cloud['vpc_id'] is not None:
-        # boto_kwargs['vpc_id'] = cloud['vpc_id']
+        if spec.user_data is not None:
+            boto_kwargs['user_data'] = spec.user_data
+        if spec.security_group_name is not None:
+            boto_kwargs['security_groups'] = [spec.security_group_name]
+        if spec.security_group_id is not None:
+            boto_kwargs['security_group_ids'] = [spec.security_group_id]
+        if spec.subnet_id is not None:
+            boto_kwargs['subnet_id'] = spec.subnet_id
+        # if spec.vpc_id is not None:
+        #     boto_kwargs['vpc_id'] = spec.vpc_id
 
-        # Run
         try:
-            reservation = cloud['conn'].run_instances(ami_id, **boto_kwargs)
+            reservation = self.auth.get_conn().run_instances(
+                spec.ami_id, **boto_kwargs
+            )
             self.logger.debug(
                 'Reservation {0.id:s} for {1:d} instances was created'.format(
                     reservation, amt
@@ -102,77 +110,18 @@ class EC2Client(object):
             self.logger.error('Failed to run instances: {}'.format(str(err)))
             return None
 
-    def wait(self, timeout=120, cloud_id=None):
-        """Wait for instances of current session to come up.
-
-        :param int timeout: The number of seconds to wait before returning
-            `False`.
-        :param str cloud_id: The id of the cloud to use.
-        :rtype: bool
-        :return: `False` if timeout was exceeded. `True` if everything went
-            well.
-        """
-        # Look for cloud
-        if cloud_id is None:
-            cloud_id = self.default_cloud_id
-        cloud = self.clouds.get(cloud_id, None)
-        if cloud is None:
-            self.logger.error('Failed to use cloud {}'.format(cloud_id))
-            return False
-
-        # Check reservations
-        try:
-            seconds = 0
-            while seconds < timeout:
-                reservations = cloud['conn'].get_all_instances()
-
-                loop_again = False
-                for reservation in reservations:
-                    for instance in reservation.instances:
-                        if instance.state == 'pending':
-                            self.logger.debug(
-                                'Instance {0.id} is in status pending...'
-                                .format(instance)
-                            )
-                            loop_again = True
-                            break
-                    if loop_again:
-                        break
-                if loop_again:
-                    seconds += 5
-                    self.logger.debug('Sleeping for 5 seconds...')
-                    time.sleep(5)
-                    continue
-                return True
-
-            return False
-        except boto_exception.EC2ResponseError as err:
-            self.logger.error(
-                'Failed to check on instances: {}'.format(str(err)))
-            return False
-
-    def destroy(self, instance_ids=None, cloud_id=None):
+    def destroy(self, instance_ids=None):
         """Wait for instances of current session to come up.
 
         :param list of [str] instance_ids: List of instances to destroy. If not
             provided, all the instances in given cloud and session will be
             destroyed.
-        :param str cloud_id: The id of the cloud to use.
-        :rtype: list of [boto.ece2.instance.Instance]
         :return: List of destroyed instances (`boto.ece2.instance.Instance`).
+        :rtype: list of [boto.ece2.instance.Instance]
         """
-        # Look for cloud
-        if cloud_id is None:
-            cloud_id = self.default_cloud_id
-        cloud = self.clouds.get(cloud_id, None)
-        if cloud is None:
-            self.logger.error('Failed to use cloud {}'.format(cloud_id))
-            return []
-
         # Get list of instance ids
         if instance_ids is None or len(instance_ids) == 0:
-            # Load all instances
-            reservations = cloud['conn'].get_all_instances()
+            reservations = self.auth.get_conn().get_all_instances()
             instance_ids = []
             for reservation in reservations:
                 for inst in reservation.instances:
@@ -182,9 +131,8 @@ class EC2Client(object):
                         continue
                     instance_ids.append(inst.id)
 
-        # Destroy instances
         if len(instance_ids) > 0:
-            instances = cloud['conn'].terminate_instances(instance_ids)
+            instances = self.auth.get_conn().terminate_instances(instance_ids)
         else:
             instances = []
         self.logger.debug(
@@ -193,24 +141,14 @@ class EC2Client(object):
 
         return instances
 
-    def get_list(self, cloud_id=None):
+    def get_list(self):
         """Returns list of current reservations.
 
-        :param str cloud_id: The id of the cloud to use.
-        :rtype: list of [boto.ece2.instance.Reservation]
         :return: List of active reservations (`boto.ece2.instance.Reservation`).
+        :rtype: list of [boto.ece2.instance.Reservation]
         """
-        # Look for cloud
-        if cloud_id is None:
-            cloud_id = self.default_cloud_id
-        cloud = self.clouds.get(cloud_id, None)
-        if cloud is None:
-            self.logger.error('Failed to use cloud {}'.format(cloud_id))
-            return []
-
-        # Run
         try:
-            reservations = cloud['conn'].get_all_instances()
+            reservations = self.auth.get_conn().get_all_instances()
             self.logger.debug(
                 'Found {0:d} reservations'.format(len(reservations))
             )
@@ -218,79 +156,3 @@ class EC2Client(object):
         except boto_exception.EC2ResponseError as err:
             self.logger.error('Failed to list instances: {}'.format(str(err)))
             return None
-
-    #
-    # Helpers
-    #
-
-    def _parse_cloud_configuration(self):
-        # Fetch cloud ids
-        tci = self.config.get_str('ec2', 'clouds', '').split(',')
-        cloud_ids = []
-        for c in tci:  # remove empty string
-            if c != '':
-                cloud_ids.append(c)
-        if len(cloud_ids) == 0:
-            return {}, None
-
-        # Credentials
-        aws_access_key_id = self.config.get_str('ec2', 'aws_access_key', '')
-        aws_secret_key = self.config.get_str('ec2', 'aws_secret_key', '')
-
-        # Initialize connections
-        clouds = {}
-
-        for cloud_id in cloud_ids:
-            cloud = {}
-
-            # Load configuration
-            cloud_cfg = self.config.get_dict('ec2_{}'.format(cloud_id))
-
-            # Select default flavor
-            cloud['default_flavor'] = cloud_cfg.get(
-                'default_flavor',
-                self.config.get_str('ec2', 'default_flavor', 't2.micro')
-            )
-
-            # Initialize Boto connection
-            try:
-                # Load configuration
-                cloud['sg_id'] = cloud_cfg.get('security_group_id', None)
-                cloud['sg_name'] = cloud_cfg.get('security_group_name', None)
-                cloud['subnet_id'] = cloud_cfg.get('subnet_id', None)
-                cloud['vpc_id'] = cloud_cfg.get('vpc_id', None)
-                cloud['ssh_key_name'] = cloud_cfg['ssh_key_name']
-                cloud['default_ami_id'] = cloud_cfg['default_ami_id']
-
-                # URL
-                if 'region' in cloud_cfg:
-                    ec2_url = cloud_cfg['region']
-                elif 'url' in cloud_cfg:
-                    ec2_url = cloud_cfg['url']
-                else:
-                    ec2_url = cloud_id
-
-                # Initiate connection
-                cloud['conn'] = ec2.connect_to_region(
-                    ec2_url,
-                    aws_access_key_id=aws_access_key_id,
-                    aws_secret_access_key=aws_secret_key
-                )
-                if cloud['conn'] is None:  # Connection failed!
-                    self.logger.error(
-                        'Failed to setup Boto connection with'
-                        + ' cloud `{}`'.format(cloud_id)
-                    )
-                    continue
-            except KeyError as err:  # a configuration option is missing
-                self.logger.error(
-                    'Option `{}` not found in `ec2_{}` section!'.format(
-                        str(err.args[0]), cloud_id
-                    )
-                )
-                continue
-
-            # Append cloud to list
-            clouds[cloud_id] = cloud
-
-        return clouds, cloud_ids[0]
--- test/ice/test_ec2_client.py
+++ test/ice/test_ec2_client.py
@@ -1,7 +1,9 @@
+import logging
 import unittest2
 import mock
 from ice import ec2_client
 from boto import ec2 as boto_ec2
+from boto import exception as boto_exception
 
 
 class TestEC2CloudAuth(unittest2.TestCase):
@@ -34,3 +36,160 @@ class TestEC2CloudAuth(unittest2.TestCase):
         self.assertEqual(conn_a, conn_b)
 
         self.assertEqual(boto_ec2.connect_to_region.call_count, 1)
+
+
+class TestEC2Client(unittest2.TestCase):
+    def setUp(self):
+        self.cloud_auth = ec2_client.EC2CloudAuth(
+            'banana.aws.com',
+            'IAmABanana',
+            'IAmASecretBanana'
+        )
+
+        self.ec2_conn = mock.MagicMock()
+        self.cloud_auth.get_conn = mock.MagicMock(return_value=self.ec2_conn)
+
+        self.ec2_client = ec2_client.EC2Client(
+            self.cloud_auth, logging.getLogger('testing')
+        )
+
+
+class TestEC2ClientGetList(TestEC2Client):
+    def setUp(self):
+        TestEC2Client.setUp(self)
+
+        self.fake_result = [
+            {"fake": "reservation-1"},
+            {"fake": "reservation-2"}
+        ]
+        self.ec2_conn.get_all_instances = mock.MagicMock(
+            return_value=self.fake_result
+        )
+
+    def test_get_list(self):
+        self.assertListEqual(self.ec2_client.get_list(), self.fake_result)
+        self.ec2_conn.get_all_instances.assert_called_once_with()
+
+    def test_error(self):
+        self.ec2_conn.get_all_instances.side_effect = \
+            boto_exception.EC2ResponseError(500, 'banana')
+        self.assertIsNone(self.ec2_client.get_list())
+
+
+class TestEC2ClientDestroy(TestEC2Client):
+    def setUp(self):
+        TestEC2Client.setUp(self)
+
+        reservations = [
+            mock.MagicMock(
+                instances=[
+                    mock.MagicMock(id='a', state='running'),
+                    mock.MagicMock(id='b', state='terminated')
+                ]
+            ),
+            mock.MagicMock(
+                instances=[
+                    mock.MagicMock(id='c', state='starting'),
+                    mock.MagicMock(id='d', state='shutting-down')
+                ]
+            )
+        ]
+        self.ec2_conn.get_all_instances = mock.MagicMock(
+            return_value=reservations
+        )
+
+        self.fake_result = [
+            mock.MagicMock(
+                instances=[mock.MagicMock(id='a', state='shutting-down')]
+            ),
+            mock.MagicMock(
+                instances=[mock.MagicMock(id='c', state='shutting-down')]
+            )
+        ]
+        self.ec2_conn.terminate_instances = mock.MagicMock(
+            return_value=self.fake_result
+        )
+
+    def test_destroy_with_instance_ids(self):
+        self.assertListEqual(
+            self.ec2_client.destroy(['e', 'f']),
+            self.fake_result
+        )
+        self.ec2_conn.terminate_instances.assert_called_once_with(['e', 'f'])
+
+    def test_destroy_without_instance_ids(self):
+        self.assertListEqual(
+            self.ec2_client.destroy(),
+            self.fake_result
+        )
+        self.ec2_conn.terminate_instances.assert_called_once_with(['a', 'c'])
+
+    def test_destroy_with_no_instances(self):
+        self.ec2_conn.get_all_instances = mock.MagicMock(return_value=[])
+        self.assertListEqual(self.ec2_client.destroy(), [])
+        self.ec2_conn.terminate_instance.assert_not_called()
+
+
+class TestEC2ClientCreate(TestEC2Client):
+    def setUp(self):
+        TestEC2Client.setUp(self)
+
+        self.fake_result = mock.MagicMock(id='r-123456')
+        self.ec2_conn.run_instances = mock.MagicMock(
+            return_value=self.fake_result
+        )
+
+    def test_create(self):
+        spec = ec2_client.EC2VMSpec(
+            'm-123456',
+            'key-1234567',
+            flavor='m3.large',
+            user_data='echo 123',
+            security_group_id='sg-123456',
+            security_group_name='sn-123456',
+            subnet_id='sub-123456'
+        )
+        self.assertEqual(
+            self.ec2_client.create(10, spec),
+            self.fake_result
+        )
+
+        self.assertEqual(self.ec2_conn.run_instances.call_count, 1)
+        self.ec2_conn.run_instances.assert_has_calls(
+            [mock.call(
+                spec.ami_id,
+                instance_type=spec.flavor,
+                key_name=spec.ssh_key_name,
+                min_count=10,
+                max_count=10,
+                security_group_ids=[spec.security_group_id],
+                security_groups=[spec.security_group_name],
+                subnet_id=spec.subnet_id,
+                user_data=spec.user_data
+            )]
+        )
+
+    def test_create_with_defaults(self):
+        spec = ec2_client.EC2VMSpec('m-123456', 'key-123456')
+        self.assertEqual(
+            self.ec2_client.create(10, spec),
+            self.fake_result
+        )
+
+        self.assertEqual(self.ec2_conn.run_instances.call_count, 1)
+        self.ec2_conn.run_instances.assert_has_calls(
+            [mock.call(
+                spec.ami_id,
+                instance_type='t2.micro',
+                key_name=spec.ssh_key_name,
+                min_count=10,
+                max_count=10
+            )]
+        )
+
+    def test_error(self):
+        spec = ec2_client.EC2VMSpec('m-123456', 'key-123456')
+        self.ec2_conn.run_instances.side_effect = \
+            boto_exception.EC2ResponseError(500, 'banana')
+
+        self.assertIsNone(self.ec2_client.create(2, spec))
