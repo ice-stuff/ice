9c5d78c1f5e616539e6bce6273e014265c946a5b
George Lestaris <glestaris@gmail.com>
2015-08-22 13:12:38 +0100

    Release v1.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
     # Metadata
     name="iCE",
-    version="1.2.1",
+    version="1.3.0",
     author="George Lestaris",
     author_email="glestaris@gmail.com",
     description="Interactive cloud experiments and monitoring",
