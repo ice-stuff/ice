f085f67d811da2ba70916fdec8deffcc676ca1d1
George Lestaris <glestaris@gmail.com>
2015-09-17 19:52:16 +0100

    Fix ice-server with new server

--- ice/__init__.py
+++ ice/__init__.py
@@ -1,4 +1,32 @@
 """Entry point for the iCE application."""
+import os
+import sys
+
+#
+# iCE experiment API
+#
+
 from .tasks import Callable, Runner, Task, ParallelTask
 
+#
+# iCE Version
+#
+
 __version__ = '2.0.0'
+
+#
+# Configuration directories list
+#
+
+CONFIG_DIRS = [
+    os.path.join(sys.prefix, 'etc', 'ice'),
+    os.path.join(sys.prefix, 'local', 'etc', 'ice'),
+    os.path.join('/etc/ice'),
+    os.path.expanduser('~/.ice')
+]
+env_paths_str = os.environ.get('ICE_CONFIG_PATHS')
+if env_paths_str is not None:
+    env_paths = env_paths_str.split(':')
+    for path in env_paths:
+        CONFIG_DIRS.append(os.path.abspath(path))
+
