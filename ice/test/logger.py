2ab6768ef10c5bf35cdf8b5e2dab4fc198b8786e
George Lestaris <glestaris@gmail.com>
2016-10-08 12:26:28 +0100

    Don't set a handler to test loggers
    Nose takes care of printing the log lines if a test fails.

--- ice/test/logger.py
+++ ice/test/logger.py
@@ -9,16 +9,4 @@ def get_dummy_logger(name):
     if os.environ.get('TEST_ICE_DEBUG', '') == '1':
         logger.setLevel(logging.DEBUG)
 
-    ch = logging.StreamHandler()
-    formatter = logging.Formatter(
-        '%(asctime)s [%(levelname)s] %(name)s :: %(message)s'
-    )
-    ch.setFormatter(formatter)
-
-    ch.setLevel(logging.INFO)
-    if os.environ.get('TEST_ICE_DEBUG', '') == '1':
-        ch.setLevel(logging.DEBUG)
-
-    logger.addHandler(ch)
-
     return logger
