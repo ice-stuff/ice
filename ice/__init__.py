8ba5b3d7e4e73dc6f462625841de4e1dffb947b8
George Lestaris <glestaris@gmail.com>
2016-10-30 19:55:33 +0000

    Don't define configuration dirs in this repo

--- ice/__init__.py
+++ ice/__init__.py
@@ -13,20 +13,3 @@ from .tasks import Callable, Runner, ParallelRunner, Task, ParallelTask
 #
 
 __version__ = '2.1.0-dev'
-
-#
-# Configuration directories list
-#
-
-CONFIG_DIRS = [
-    os.path.join(sys.prefix, 'etc', 'ice'),
-    os.path.join(sys.prefix, 'local', 'etc', 'ice'),
-    os.path.join('/etc/ice'),
-    os.path.expanduser('~/.ice')
-]
-env_paths_str = os.environ.get('ICE_CONFIG_PATHS')
-if env_paths_str is not None:
-    env_paths = env_paths_str.split(':')
-    for path in env_paths:
-        CONFIG_DIRS.append(os.path.abspath(path))
-
